b68133746b98cd67cdebd0706fcda3ea868fe515
luizgpp <luizgpp@gmail.com>
2019-05-21 20:31:49 -0300

    adicionado links no menu somente para usuarios logados - não foi tratos roles e permissions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Consultorio') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,18 +23,35 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Consultorio') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                @auth
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item {{ Request::is('home') ? 'active' : ''}}">
+                        <a href="" class="nav-link ">Consultas</a>
+                        </li>     
+                        <li class="nav-item {{ Request::is('clientes') ? 'active' : ''}}">
+                                <a href="" class="nav-link">Clientes</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('procedimentos') ? 'active' : ''}}">
+                                <a href="" class="nav-link">Procedimentos</a>
+                        </li>                     
+                        <li class="nav-item {{ Request::is('produtos') ? 'active' : ''}}">
+                            <a href="" class="nav-link">Produtos</a>
+                        </li>                        
+                        <li class="nav-item {{ Request::is('colaboradores') ? 'active' : ''}}">
+                            <a href="" class="nav-link ">Colaboradores</a>
+                        </li>                        
                     </ul>
+                    @endauth
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -49,6 +66,7 @@
                                 </li>
                             @endif
                         @else
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
