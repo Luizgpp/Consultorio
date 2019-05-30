8eaf101c0ffe3d2d2abc8f3efc1d5f67b4e7cd37
luizgpp <luizgpp@gmail.com>
2019-05-30 19:02:07 -0300

    Criado model Cargo, Colaborador e Telefone

--- app/Colaborador.php
+++ app/Colaborador.php
@@ -6,8 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Colaborador extends Model
 {
+    protected $table = "colaboradores";
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function cargo()
+    {
+        return $this->belongsTo(Cargo::class);
+    }
+
+    public function telefones()
+    {
+        return $this->hasMany(Telefone::class, 'colaboradores_id');
     }
 }
