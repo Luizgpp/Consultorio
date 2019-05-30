23c0621ea3824b2657274ef8c309a9a7f6e58a55
luizgpp <luizgpp@gmail.com>
2019-05-30 19:03:32 -0300

    criado migration Telefones

---
+++ database/migrations/2019_05_11_184769_create_telefones_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTelefonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telefones', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ddd');
+            $table->string('numero');
+            $table->string('tipo');
+            $table->bigInteger('colaboradores_id')->unsigned();
+
+            $table->foreign('colaboradores_id')
+                ->references('id')
+                ->on('colaboradores')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telefones');
+    }
+}
